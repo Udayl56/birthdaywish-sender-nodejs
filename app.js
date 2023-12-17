61a548f7ef0d195302ab5b097340a9e5ff68fb34
Uday Ladhi <105300620+Udayl56@users.noreply.github.com>
2023-12-17 16:11:07 +0530

    Update app.js
    email fix

--- app.js
+++ app.js
@@ -12,16 +12,16 @@ runQuery()
         for (const person of result) {
             const mailOptions = {
                 from: process.env.SMTP_USER,
-                to: person.email, 
+                to: person.Email, 
                 subject: 'Happy Birthday!',
-                text: `Dear ${person.name},\n\nHappy birthday! Wishing you a fantastic day filled with joy and happiness.\n\nBest regards,\nUday Ladhi`,
+                text: `Dear ${person.Name},\n\nHappy birthday! Wishing you a fantastic day filled with joy and happiness.\n\nBest regards,\nUday Ladhi`,
             };
 
             smtpTransport.sendMail(mailOptions, (error, info) => {
                 if (error) {
                     console.error('Error sending email:', error);
                 } else {
-                    console.log('Email sent to ${person.name}:', info.response);
+                    console.log('Email sent to ${person.Name}:', info.response);
                 }
             });
         }
